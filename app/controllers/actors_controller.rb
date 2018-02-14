1c180fe3c537a744b6c21d660331f5de279227b8
Murugan <murugan@firstdraft.com>
2018-02-14 03:29:02 +0000

    MSM Debug full score

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -21,6 +21,7 @@ class ActorsController < ApplicationController
     @actor.dob = params.fetch("dob")
     @actor.name = params.fetch("name")
     @actor.image_url = params.fetch("image_url")
+    @actor.bio = params.fetch("bio")
     @actor.save
 
     redirect_to("/actors", :notice => "Actor created successfully.")
